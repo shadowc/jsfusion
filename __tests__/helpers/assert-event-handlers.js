2de7df3dc2a68027944d862eb2ea2e555c7db1c0
shadowc <info@matiasjose.com>
2022-03-13 01:19:05 +0100

    test suite

---
+++ __tests__/helpers/assert-event-handlers.js
@@ -0,0 +1,24 @@
+/**
+ * @param {object} component
+ * @param {{eventName: string, callback: function, target: HTMLElement}[]} options
+ */
+export const assertEventHandlers = (component, options) => {
+    expect(component.eventHandlers.length).toBe(options.length);
+
+
+    options.forEach((option) => {
+        let found = false;
+        for (let i = 0; i < component.eventHandlers.length; i++) {
+            const eventHandler = component.eventHandlers[i];
+            if (
+                eventHandler.eventName === option.eventName
+                && eventHandler.callback === option.callback
+                && eventHandler.target === option.target
+            ) {
+                found = true;
+                break;
+            }
+        }
+        expect(found).toBe(true);
+    });
+}
